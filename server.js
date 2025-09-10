187deda3142b193252e8646e18cf98e7792d582a
shubhro11 <shubhro.seby11@gmail.com>
2025-09-10 20:40:50 +0530

    Set up database connection with Mongoose and dotenv configuration

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
+require("dotenv").config()
 const express = require("express")
+const connectToDB = require("./src/db/db")
 
+connectToDB()
 const app = require("./src/app")
 
+app.use(express.json())
 
 
 app.listen(3000, ()=> console.log("Server is running on Port 3000."))
--- src/db/db.js
+++ src/db/db.js
@@ -1,3 +1,9 @@
 const mongoose = require("mongoose")
 
+function connectToDB () {
+    mongoose.connect(process.env.MONGODB_URI)
+    .then(()=> console.log("Connected to DB"))
+    .catch((err)=> console.log("Error connecting to DB: ", err))
+}
 
+module.exports = connectToDB
